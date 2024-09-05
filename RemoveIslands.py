8bffa5ef18d4c3fde7b3b02d38341d2c6a1df947
Varun Totakura <totakura.varun@gmail.com>
2024-09-05 10:03:42 -0400

    Remove Islands Optimized

--- RemoveIslands.py
+++ RemoveIslands.py
@@ -8,21 +8,53 @@ def is_outside(i, j, matrix):
         return True
     return False
 
+def make_key(i, j):
+    return f"{i}{j}"
+
 def check_around(i, j, matrix, border_islands):
-    steps = {(0, 1), (1, 0), (0, -1), (-1, 0)}
+    steps = [(0, 1), (1, 0), (0, -1), (-1, 0)]
     for (ix, jx) in steps:
         new_i = i + ix
         new_j = j + jx
-        if is_border(new_i, new_j, matrix) and not is_outside(new_i, new_j, matrix):
-            border_islands[new_i, new_j] = True
+        if is_outside(new_i, new_j, matrix):
+            continue
+        neigh = matrix[new_i][new_j]
+        key = make_key(new_i, new_j)
+        if neigh == 1 and not key in border_islands:
+            border_islands[key] = True
             check_around(new_i, new_j, matrix, border_islands)
-    return border_islands
 
 def removeIslands(matrix):
     border_islands = {}
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
-            if is_border(i, j, matrix):
-                border_islands[i, j] = True
-                border_islands = check_around(i, j, matrix, border_islands)
+            if matrix[i][j] == 1 and is_border(i, j, matrix):
+                if make_key(i, j) in border_islands:
+                    continue
+                border_islands[make_key(i, j)] = True
+                check_around(i, j, matrix, border_islands)
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if matrix[i][j] == 1 and not make_key(i, j) in border_islands:
+                matrix[i][j] = 0
     return matrix
+
+matrix = [
+    [1, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 1, 1],
+    [0, 0, 1, 0, 1, 0],
+    [1, 1, 0, 0, 1, 0],
+    [1, 0, 1, 1, 0, 0],
+    [1, 0, 0, 0, 0, 1]
+]
+
+output = [
+    [1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 1],
+    [0, 0, 0, 0, 1, 0],
+    [1, 1, 0, 0, 1, 0],
+    [1, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 1]
+]
+
+print(removeIslands(matrix) == output)
